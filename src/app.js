d304934599049e5132fcb852ffd89c21f906b83e
Ken Leong <kenleong1@gmail.com>
2021-07-05 22:35:32 +0800

    module4

--- src/app.js
+++ src/app.js
@@ -59,14 +59,14 @@ app.get('/transfer', (req, res) => {
 });
 
 app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= parseInt(req.body.amount);
-    accounts[req.body.to].balance += parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts);
+    accounts[req.body.from].balance -= parseInt(req.body.amount, 10);
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'UTF8');
     res.render('transfer',
-    {
-        message: "Transfer Completed"
-    });
+        {
+            message: "Transfer Completed"
+        });
 });
 
 app.get('/payment', (req, res) => {
@@ -77,18 +77,16 @@ app.get('/payment', (req, res) => {
 });
 
 app.post('/payment', (req, res) => {
-    accounts.credit.balance -= parseInt(req.body.amount);
-    accounts.credit.available  += parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts);
+    accounts.credit.balance -= parseInt(req.body.amount, 10);
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'UTF8');
     res.render('payment',
-    {
-        message: "Payment Successful",
-        account: accounts.credit
-    });
+        {
+            message: "Payment Successful",
+            account: accounts.credit
+        });
 });
-
-
 
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!");
