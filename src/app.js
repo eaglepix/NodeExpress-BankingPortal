ae79735203387c095c44e4d3fef633fe7a99c036
Ken Leong <kenleong1@gmail.com>
2021-07-05 16:30:05 +0800

    module2

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.set(views, path.join(__dirname, "src"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -12,5 +12,7 @@ app.get('/', (req,res) =>{
     res.render('index', {title: 'Index'})
 });
 
-app.listen(3000, "PS Project Running on port 3000!");
+app.listen(3000, ()=>{
+    console.log("PS Project Running on port 3000!");
+} );
 
